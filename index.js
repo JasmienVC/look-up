c31e75dcd2dff2376246fd3ccd938282f231d40c
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2024-05-06 20:02:50 +0200

    multiple poops, game over function and screen image added

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ class Meadow {
 class Man {
   constructor() {
     this.x = 0;
-    this.y = 390;
+    this.y = 500;
 
     const img = new Image();
     img.src = "./images/man.svg";
@@ -52,15 +52,16 @@ class Man {
 
 class Poop {
   constructor() {
-    this.x = Math.random() * window.innerWidth;
+    this.x = Math.random() * canvas.width;
     this.y = 0;
-    this.vy = 4;
-    this.gravity = 1;
+    this.vy = 1;
+    this.gravity = Math.random() / 4;
+    this.interval = setInterval(fallingPoop, 50);
   }
 
   draw() {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 20, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, 15, 0, Math.PI * 2);
     ctx.fillStyle = "#622711";
     ctx.fill();
     ctx.closePath();
@@ -69,7 +70,11 @@ class Poop {
 
 const meadow = new Meadow();
 const man = new Man();
-const poop = new Poop();
+const poops = [];
+
+for (let i = 0; i < 3; i++) {
+  poops.push(new Poop());
+}
 
 document.addEventListener("keydown", e => {
   switch (e.keyCode) {
@@ -84,16 +89,43 @@ document.addEventListener("keydown", e => {
 });
 
 function updateCanvas() {
-  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   meadow.draw();
   man.draw();
+  checkGameOver();
+}
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function fallingPoop() {
   updateCanvas();
-  poop.draw();
-  poop.y += poop.vy;
-  poop.vy += poop.gravity;
+  poops.forEach(function(poop) {
+    poop.draw();
+    poop.y += poop.vy;
+    poop.vy += poop.gravity;
+
+    if (poop.y > canvas.height) {
+      poop.x = Math.random() * canvas.width;
+      poop.y = 0;
+      poop.vy = 1;
+      poop.gravity = Math.random() / 4;
+    }
+  });
 }
 
-setInterval(fallingPoop, 50)
+function checkGameOver() {
+  const gameOver = new Image();
+  gameOver.src = "./images/gameover.jpg";
+
+  poops.some(function (poop) {
+    if (poop.y >= man.y && poop.x + 15 >= man.x + 145 && man.x + 255 >= poop.x - 15) {
+      gameOver.addEventListener("load", () => {
+        poops.forEach(poop => clearInterval(poop.interval));
+        clearCanvas();
+        ctx.drawImage(gameOver, 0, 0, canvas.width, canvas.height);
+      })
+    }
+  });
+}
