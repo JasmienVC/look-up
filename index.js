a9b1ab6c96fd92865275e0b99fae3d14c7767a1e
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2024-06-03 10:10:36 +0200

    refactoring wip

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const ctx = canvas.getContext("2d");
+const game = {
+  canvas: document.getElementById("canvas"),
+  status: "",
+  level: 1,
+  setup: function () {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    this.ctx = this.canvas.getContext("2d");
+  },
+  clear: function () {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  printLevel: function () {
+    this.ctx.fillStyle = "black";
+    this.ctx.font = "30px Arial";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText(`Level ${  this.level}`, 75, 50);
+  },
+  printPlayAgain: function () {
+    this.ctx.fillStyle = "white";
+    this.ctx.font = "30px Arial";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText(`Level ${this.level}`, 75, 50);
+    this.ctx.font = "60px Arial";
+    this.ctx.textAlign = "center"
+    this.ctx.fillText(
+      "Press enter to play again", this.canvas.width / 2, this.canvas.height - 100
+    );
+  }
+}
 
 class Meadow {
   constructor() {
@@ -16,14 +42,14 @@ class Meadow {
   }
 
   draw() {
-    ctx.drawImage(this.img, 0, 0, window.innerWidth, window.innerHeight);
+    game.ctx.drawImage(this.img, 0, 0, game.canvas.width, game.canvas.height);
   }
 }
 
 class Man {
   constructor() {
     this.x = 0;
-    this.y = canvas.height - 300;
+    this.y = game.canvas.height - 300;
     this.level = setInterval(levelUp, 10000)
 
     const img = new Image();
@@ -44,13 +70,13 @@ class Man {
   }
 
   draw() {
-    ctx.drawImage(this.img, this.x, this.y, 400, 300);
+    game.ctx.drawImage(this.img, this.x, this.y, 400, 300);
   }
 }
 
 class Poop {
   constructor() {
-    this.x = Math.random() * canvas.width;
+    this.x = Math.random() * game.canvas.width;
     this.y = 0;
     this.vy = 4;
     this.gravity = Math.random() / 2;
@@ -63,10 +89,11 @@ class Poop {
   }
 
   draw() {
-    ctx.drawImage(this.img, this.x, this.y, 50, 50);
+    game.ctx.drawImage(this.img, this.x, this.y, 50, 50);
   }
 }
 
+game.setup();
 const meadow = new Meadow();
 const man = new Man();
 const poops = [];
@@ -75,20 +102,20 @@ poops.push(new Poop());
 document.addEventListener("keydown", e => {
   switch (e.keyCode) {
     case 37:
-      if(man.x > -125 && gameStatus !== "lost") {
+      if(man.x > -125 && game.status !== "lost") {
         man.moveLeft();
         updateCanvas();
       }
       break;
     case 39:
-      if(man.x < canvas.width - 300 && gameStatus !== "lost") {
+      if(man.x < game.canvas.width - 300 && game.status !== "lost") {
         man.moveRight();
         updateCanvas();
       }
       break;
     case 13:
-      level = 1;
-      gameStatus = "";
+      game.level = 1;
+      game.status = "";
       poops.forEach(poop => poops.splice(poop));
       poops.push(new Poop());
       man.level = setInterval(levelUp, 10000)
@@ -98,15 +125,11 @@ document.addEventListener("keydown", e => {
 });
 
 function updateCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  game.clear();
   meadow.draw();
   man.draw();
-  printLevel();
+  game.printLevel();
   checkGameOver();
-}
-
-function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function fallingPoop() {
@@ -116,8 +139,8 @@ function fallingPoop() {
     poop.y += poop.vy;
     poop.vy += poop.gravity;
 
-    if (poop.y > canvas.height) {
-      poop.x = Math.random() * canvas.width;
+    if (poop.y > game.canvas.height) {
+      poop.x = Math.random() * game.canvas.width;
       poop.y = 0;
       poop.vy = 4;
       poop.gravity = Math.random() / 2;
@@ -125,47 +148,26 @@ function fallingPoop() {
   });
 }
 
-let gameStatus = ""
-
 function checkGameOver() {
   const gameOver = new Image();
   gameOver.src = "./images/gameover.jpg";
 
   poops.some(function (poop) {
     if (poop.y >= man.y && poop.x + 15 >= man.x + 145 && man.x + 255 >= poop.x - 15) {
-      gameStatus = "lost"
+      game.status = "lost"
       gameOver.addEventListener("load", () => {
         poops.forEach(poop => clearInterval(poop.interval));
-        clearCanvas();
-        ctx.drawImage(gameOver, 0, 0, canvas.width, canvas.height);
+        game.clear();
+        game.ctx.drawImage(gameOver, 0, 0, game.canvas.width, game.canvas.height);
         clearInterval(clearInterval(man.level));
-        printPlayAgain();
+        game.printPlayAgain();
       })
     }
   });
 }
 
-let level = 1
-
 function levelUp() {
-  level += 1;
+  game.level += 1;
   poops.push(new Poop());
   updateCanvas();
-}
-
-function printLevel() {
-  ctx.fillStyle = "black";
-  ctx.font = "30px Arial";
-  ctx.textAlign = "center";
-  ctx.fillText(`Level ${level}`, 75, 50);
-}
-
-function printPlayAgain() {
-  ctx.fillStyle = "white";
-  ctx.font = "30px Arial";
-  ctx.textAlign = "center";
-  ctx.fillText(`Level ${level}`, 75, 50);
-  ctx.font = "60px Arial";
-  ctx.textAlign = "center"
-  ctx.fillText("Press enter to play again", canvas.width / 2, canvas.height - 100);
 }
